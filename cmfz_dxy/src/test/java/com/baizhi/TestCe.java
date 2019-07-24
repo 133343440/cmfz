3251933072015abb7f04e4fc670748c239a4821c
Your Name <133343440@vip.qq.com>
2019-07-24 11:28:17 +0800

    这是第二次提交

---
+++ cmfz_dxy/src/test/java/com/baizhi/TestCe.java
@@ -0,0 +1,15 @@
+package com.baizhi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class TestCe {
+    @Test
+    public  void test1(){
+        System.out.println("测试一下");
+    }
+}
